5fdf27b3e6f5a61039b72c2a5015102842309ae0
André Castillo <hola@derianandre.com>
2021-10-08 20:43:13 -0600

    Update conversions.js

--- calculator/units/conversions.js
+++ calculator/units/conversions.js
@@ -2,9 +2,9 @@
  * Conversion module
  * Convert a number from one unit to another.
  * @author Derian Castillo
- * @version 1.0.0
+ * @version 1.1.0
  * @dateCreated 2021-09-24
- * @dateUpdated 2021-09-24
+ * @dateUpdated 2021-10-08
  */
 
 // Errors
@@ -14,16 +14,19 @@ import {
 } 
 from '~/calculator/errors';
 
-// Conversion by ratio
+// Conversion by formula or ratio
 export function conversion(input) {
 	var { formula, ratio, units, value } = input;
+	// Convert to numbers
 	value = Number(value);
 	ratio = Number(ratio);
-	console.log(typeof value, value)
+	// Check if the input is a number
 	if(typeof value != 'number' || isNaN(value)) {
 		return { error: errorInputValueNotANumber };
 	}
+	// Check if the formula is valid
 	try {
+		// If the formula is not defined, use the ratio
 		value = formula ? eval(formula) : (value * ratio);
 	} catch (error) {
 		return { error: errorDefault };
